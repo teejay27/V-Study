29a0961f61f8f18159d483c870e2f487ee417877
tjay27 <tejaswigurram27@gmail.com>
2022-05-24 11:37:40 +0530

    changed script file

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ app.use(express.static('public'))
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
-app.listen(8000,()=>{
+app.listen(process.env.port||8000,()=>{
     console.log("listening on 8000");
 })
